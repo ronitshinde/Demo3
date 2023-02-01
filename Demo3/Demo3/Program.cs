72f70ebefdfb722396f70f3189c9cb94d83c1429
ronitshinde <rony.shinde753@gmail.com>
2023-02-01 17:46:02 +0530

    [Ronit]Add : UC1

---
+++ Demo3/Demo3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo3
+{
+    public class Program
+    {
+         int a;
+        public static void Main(string[] args)
+        {
+            //Test.MyMethods1();
+            //Test Myobj = new Test();
+            //Myobj.MyMethods2();
+            //Console.WriteLine("Hello World");
+            Program obj = new Program();
+            obj.a = 1;
+            Console.WriteLine(obj.a);
+            //Console.WriteLine(a);
+        }
+    }
+}
---
+++ Demo3/Demo3/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3
+{
+    public class Test
+    {
+        public static void MyMethods1()
+        {
+            Console.WriteLine("I am great");
+        }
+        public void MyMethods2()
+        {
+            Console.WriteLine("Life is great");
+        }
+    }
+}
